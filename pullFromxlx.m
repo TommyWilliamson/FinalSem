11c0725e9939166eca1259c3a0884257a0a7c5f3
TommyWilliamson <thomas.williamson.2018@uni.strath.ac.uk>
2023-01-23 11:49:44 +0000

    Finalised xlx code

--- pullFromxlx.m
+++ pullFromxlx.m
@@ -1,10 +1,27 @@
 function output=pullFromxlx(filename,variables)
+%% Loading Excel Table
+xlx=readtable(filename);
 
-xlx=readtable(filename)
-
-for i=1:length(variables)
-  variableName=variables(i)
-  index=find(strcmpi(xlx.Properties.VariableNames,variableName));
-  output(i)=xlx{index,2:end}
+% If only a single variable is requested...
+if size(variables,1)==1
+  index=find(strcmpi(xlx{1:end,1},variables));
+  if isempty(index)
+      disp(('Did not find: '+variables))
+  end
+  data=(xlx{index,2:end})';
+  data=data(~isnan(data))';
+  output=data;
+else
+% If multiple variables are requested...
+    for i=1:size(variables,1)
+        variableName=variables(i,:);
+        index=find(strcmpi(xlx{1:end,1},variableName));
+        if isempty(index)
+        disp(['Did not find: ',variableName])
+        end
+        data=(xlx{index,2:end})';
+        data=data(~isnan(data))';
+        output{i}=data;
+    end
 end
 end
